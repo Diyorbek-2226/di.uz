c0e8dd8ef83d073cdf8281c66f92dedf6d49b297
Diyorbek-2226 <diyorbek2226@gmail.com>
2024-05-30 14:36:30 +0500

    second commit

---
+++ src/components/Navbar.jsx/navbar.jsx
@@ -0,0 +1,14 @@
+
+// import '../Navbar/navbar.modules.css';
+
+export const Navbar = () => {
+  return (
+   <nav className="container w-[1440px] max-w-4/5 mx-auto py-5">
+    <div className="grid grid-cols-3 gap-5 max-lg:grid-cols-2 max-sm:grid-cols-1">
+      <p className="text-2xl text-teal-300">asasf</p>
+      <p>asf</p>
+      <p>afafasfasf</p>
+    </div>
+   </nav>
+  )
+}
